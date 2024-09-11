a4bceae9a13773faa9236d70aef50d6e317b56b5
ArtemSPopov <95875507+ArtemSPopov@users.noreply.github.com>
2024-09-11 23:28:50 +0300

    Write pseudocode

---
+++ script.js
@@ -0,0 +1,38 @@
+// Rock, Paper, Scissors game played in DevTools console
+
+// Greet player
+// Explain the rules
+// Initialize computer's points counter and set it to zero
+// Initialize player's points counter and set it to zero
+// Initialize round number counter and set it to zero
+// Initialize computer's choice variable?
+// Initialize player's choice variable?
+// Create a function to get computer's choice
+  // Get a random choice (1 out of 3)
+  // Convert the choice into text to present to player later
+  // Assign computer's choice to the variable
+// Create a function to get player's choice
+  // Prompt player to input their choice
+  // Assign player's choice to the variable
+// If player's choice is invalid, present the error and repeat the prompt
+// If player chose *rock*, then
+  // if computer chose *rock*, declare a tie
+  // if computer chose *paper*, declare computer as the winner of the round...
+    // ... increase computer's score
+  // if computer chose *scissors*, declare player as the winner of the round...
+    // ... increase player's score
+// If player chose *paper*, then
+  // if computer chose *rock*, declare player as the winner of the round...
+    // ... increase player's score
+  // if computer chose *paper*, declare a tie
+  // if computer chose *scissors*, declare computer as the winner of the round...
+    // ... increase computer's score
+// If player chose *scissors*, then
+  // if computer chose *rock*, declare computer as the winner of the round...
+    // ... increase computer's score
+  // if computer chose *paper*, declare player as the winner of the round...
+    // ... increase player's score
+  // if computer chose *scissors*, declare a tie
+// if computer's points are equal to 3, declare that player lost the game
+// if player's points are equal to 3, declare that player won the game
+// if no one has 3 points yet, play another round
